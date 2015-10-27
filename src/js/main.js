df64ec113898dc8af948c7e7664bd7c8a3526e23
Kristine Erickson <ericksonk09@gmail.com>
2015-10-27 14:24:33 -0400

    updated route with new html pages

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,17 @@
       .when('/login.html', {
         templateUrl: 'login.html'
       })
+      .when('/signup', {
+        templateUrl: 'signup.html'
+      })
       .when('/activity', {
         templateUrl: 'activity.html'
+      })
+      .when('/add-activity', {
+        templateUrl: 'add-activity.html'
+      })
+      .when('/edit-activity', {
+        templateUrl: 'edit-activity.html'
       })
       .when('/chart', {
         templateUrl: 'chart.html' // TODO: activity title links to chart.html

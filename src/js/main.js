da1c5b80365f1395d45a814b8dbcdd3497ffbbb2
Kristine Erickson <ericksonk09@gmail.com>
2015-10-28 23:15:08 -0400

    submits form, recieve 500 internal server error, python working on turning off authentication

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,7 @@
 
     $scope.addAct = function() {
       $http.post('https://mysterious-hollows-8407.herokuapp.com:443/api/activities/', $scope.act)
-      .then(function(){
+      .then(function($form){
         console.log("submit?");
       });
     };
